9f9ee59a7fcc2427b2bc49b5781a01a17103a86d
aramatsolrac <tamaraaselau@gmail.com>
2021-12-08 11:11:40 -0700

    Add style improvements

--- day_3/index.js
+++ day_3/index.js
@@ -1,11 +1,12 @@
 const greeting = document.getElementById("greeting")
 const btn = document.getElementById("btn")
-
+btn.addEventListener("click", snow)
 // Task:
 // - Write a function to fix the UI problems with this Christmas message (make it Christmassy!)
 // - Run the function when the Fix button is clicked.
 
-document.getElementById("btn").onclick = function fix() {
-    document.getElementById("greeting").style.font = "bold 3rem 'Mountains of Christmas', cursive";
-    document.getElementById("greeting").innerText = "🎁 Merry Christmas! 🎄"
+function snow() {
+    greeting.style.font = "bold 3rem 'Mountains of Christmas', cursive";
+    greeting.innerText = "🎁 Merry Christmas! 🎄"
+    document.body.classList.add('fix');
 }
